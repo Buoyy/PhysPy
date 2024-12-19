f47062c9ef5a2de14178c5d910ff35e793d99138
MistDragon96 <sharanparth750@gmail.com>
2024-12-19 21:34:51 +0530

    Added dot and cross product for vectors

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import vector as v
-a = v.vector(1, 1, 1)
-b = v.vector(1, 1, 2)
+a = v.vector(1, 1)
+b = v.vector(1, 1, 1)
 print("2D Vector: ", a)
 print('Magnitude: ', a.mag)
 print('Direction (w.r.t. x, y, z-axis resp.): ', (a.dir_degrees()))
@@ -10,5 +10,7 @@ print('Magnitude: ', b.mag)
 print('Direction cosines (w.r.t x, y, z axes resp.): ', (b.dir_degrees()))
 print('Normalized: ', b.normalized())
 print(a==b, a!=b, a<b, a>b, a<=b, a>=b)
+print("Dot Product:", a.dot(b))
+print("Cross Product:", a.cross(b))
 
 
--- vector.py
+++ vector.py
@@ -18,7 +18,7 @@ class vector:
     dir_degrees -- Returns a tuple with the direction of the vector with angles in degrees
     """
 
-    def __init__(self, x: float, y: float, z: float):
+    def __init__(self, x: float | None = 0 , y: float | None = 0, z: float | None = 0):
         """
         Parameters:
         ----------
@@ -54,7 +54,7 @@ class vector:
 
     def __mul__(self, other):
         """Overloads the '*' operator for a vector and a scalar for scalar multiplication."""
-        if isinstance(other, (float)):
+        if isinstance(other, (int, float)):
             return vector(self.x * other, self.y * other, self.z * other)
         elif isinstance(other, vector):
             raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}; try vector.dot() or vector.cross()')
@@ -65,31 +65,43 @@ class vector:
         """Overrides == operator for vectors. A vector is equal to another vector if all their components are equal."""
         if isinstance(other, vector):
             return bool(self.x == other.x and self.y == other.y and self.z == other.z)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
 
     def __ne__(self, other):
         """Overrides != operator for vectors. A vector is equal to another vector if all their components are equal."""
         if isinstance(other, vector):
             return (not self==other)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
 
     def __lt__(self, other):
         """Overrides < operator for vectors. A vector is less than another vector if its magnitude is less than that of other."""
         if isinstance(other, vector):
             return (self.mag < other.mag)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
 
     def __gt__(self, other):
         """Overrides > operator for vectors. A vector is more than another vector if its magnitude is more than that of other."""
         if isinstance(other, vector):
             return (self.mag > other.mag)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
 
     def __le__(self, other):
         """Overrides <= operator for vectors. A vector is less than another vector if its magnitude is more than that of other."""
         if isinstance(other, vector):
             return (self.mag < other.mag) or (self == other)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
 
     def __ge__(self, other):
         """Overrides >= operator for vectors. A vector is less than another vector if its magnitude is more than that of other."""
         if isinstance(other, vector):
             return (self.mag >= other.mag) or (self == other)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
 
     def normalized(self):
         """Returns a normalized vector."""
@@ -99,7 +111,14 @@ class vector:
         """Returns a tuple with the direction of the vector with angles in degrees."""
         return (math.degrees(self.dir[0]), math.degrees(self.dir[1]), math.degrees(self.dir[2]))
 
-    def dot(self):
-        pass
-    def cross(self):
-        pass
+    def dot(self, other):
+        if isinstance(other, vector):
+            return self.x * other.x + self.y * other.y + self.z * other.z
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
+        
+    def cross(self, other):
+        if isinstance(other, vector):
+            return vector(self.y * other.z - self.z * other.y, self.z * other.x - self.x * other.z, self.x * other.y - self.y * other.x)
+        else:
+            raise TypeError(f'Unsupported operand type(s) for : {type(self)} and {type(other)}')
